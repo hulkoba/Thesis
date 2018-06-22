de2d2c24ec8b7cc0eed8fb1374763344669ed17f
hulkoba <fuchspippi@web.de>
2018-06-22 15:55:20 +0200

    konzept: react & redux offline

---
+++ code/Form.js
@@ -0,0 +1,30 @@
+class ContactForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { contact: props.contact }
+  }
+
+  handleChange (type, event) {
+    let contact = this.state.contact
+    if (type === 'name') {
+      contact = {...contact, name: event.target.value}
+    }
+    this.setState(() => ({contact: contact}))
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.props.addOrEditContact(this.state.contact)}>
+          <label htmlFor='contact-name'>Name</label>
+          <input
+            name='contact-name'
+            value={this.state.contact.name}
+            onChange={this.handleChange.bind(this, 'name')} />
+     
+          <button onClick={this.props.handleCancel}>Cancel</button>
+          <button type='submit'>Save</button>
+      </form>
+    )
+  }
+}
+export default ContactForm
---
+++ code/Redux.js
@@ -0,0 +1,62 @@
+// action: toggle editView
+export const TOGGLE_EDIT = 'TOGGLE_EDIT'
+export function toggleEdit (contact) {
+  return {
+    type: TOGGLE_EDIT,
+    contact
+  }
+}
+export function readContacts () {
+  return {
+    type: FETCH_CONTACTS,
+    meta: {
+      offline: {
+        effect: { url: `${API}/contacts` },
+        commit: { type: 'FETCH_CONTACTS_COMMIT' },
+        rollback: { type: 'FETCH_CONTACTS_ROLLBACK' }
+      }
+    }
+  }
+}
+
+// reducer returns next app-state
+function editView (state, action) {
+  switch (action.type) {
+    case TOGGLE_EDIT:
+      return {
+        ...state,
+        isOpen: !state.isOpen,
+        contact: action.contact
+      }
+    default:
+      return state
+  }
+}
+
+function contacts (state=[], action) {
+  switch (action.type) {
+    case FETCH_CONTACTS:
+      console.log('started to fetch contacts ')
+      return state
+
+    case FETCH_CONTACTS_COMMIT:
+      console.log('successfully fetched contacts ')
+      if (state === action.payload) return state
+      return action.payload
+
+    case FETCH_CONTACTS_ROLLBACK:
+      console.log('failed to fetch contacts')
+      return state
+  }
+}
+
+import { createStore, compose } from 'redux'
+import { offline } from '@redux-offline/redux-offline'
+import offlineConfig from '@redux-offline/redux-offline/lib/defaults'
+
+const store = createStore(
+  reducer,
+  compose(
+    offline(offlineConfig)
+  )
+)
