441a40e99b05f6ff8fa9aed8cf24a9b2aa26f488
hulkoba <fuchspippi@web.de>
2018-06-28 13:11:30 +0200

    outsource detect-offline

--- code/Pouch.js
+++ code/Pouch.js
@@ -20,22 +20,22 @@ localDB.get(contact._id).then(doc => {
     return localDB.put(doc)
   })
 
-rows: [
-  doc: {
-    id: "2018-06-17T13:21:21.247Z",
-    name: "Amilia Pond",
-    email: "amilia@pond.com",
-    phone: "264542",
-    type: "cotact",
-    _id: "418b4101-a7e8-42d3-921f-610bd3c99048",
-    _rev: "1-15ad4a0100684ded8c9b6576dfe840a6"
-  },
-  id: "418b4101-a7e8-42d3-921f-610bd3c99048",
-  key: "418b4101-a7e8-42d3-921f-610bd3c99048",
-  value: {
-    rev: "1-15ad4a0100684ded8c9b6576dfe840a6"
-  }
-]
+// rows: [
+//   doc: {
+//     id: "2018-06-17T13:21:21.247Z",
+//     name: "Amilia Pond",
+//     email: "amilia@pond.com",
+//     phone: "264542",
+//     type: "cotact",
+//     _id: "418b4101-a7e8-42d3-921f-610bd3c99048",
+//     _rev: "1-15ad4a0100684ded8c9b6576dfe840a6"
+//   },
+//   id: "418b4101-a7e8-42d3-921f-610bd3c99048",
+//   key: "418b4101-a7e8-42d3-921f-610bd3c99048",
+//   value: {
+//     rev: "1-15ad4a0100684ded8c9b6576dfe840a6"
+//   }
+// ]
 
 const remoteDB = new PouchDB('http://localhost:5984/contacts')
 localDB.sync(remoteDB, [options])
