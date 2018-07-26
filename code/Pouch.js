0444c3e667a72e87c8b8726fe43f5e7b5d920901
hulkoba <fuchspippi@web.de>
2018-07-26 20:31:09 +0200

    konzept: upsert

--- code/Pouch.js
+++ code/Pouch.js
@@ -3,20 +3,26 @@ const remoteDB = new PouchDB('http://localhost:5984/contacts')
 
 localDB.sync(remoteDB, [options])
 
-// create a contact
-localDB.put({
-  ...contact,
-  _id: new Date().toISOString()
-})
-// update a contact
-localDB.get(contact._id).then(function (doc) {
-  doc = {...contact}
-  return localDB.put(doc)
-})
 // get all contacts
 localDB.allDocs({ 
   include_docs: true,
   conflicts: true
 })
+
+// create a contact
+const id = new Date().toISOString()
+await localDB.upsert(id, function () {
+  return contact
+})
+
+// update a contact
+await localDB.upsert(contact._id, function (doc) {
+  doc = {...contact}
+  return doc
+})
+
 // remove a contact
-localDB.remove(contact)
+await localDB.upsert(contact._id, function (doc) {
+  doc._deleted = true
+  return doc
+})
